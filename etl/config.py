f1b4ada6676b7c7e8cabb261c7b0d141601bcf03
Alberto Lezcano <alberto@ayax.estudio.net>
2020-04-08 16:33:32 +0200

    Code and configuration refactoring

--- etl/config.py
+++ etl/config.py
@@ -8,13 +8,63 @@ from pkg_resources import resource_filename
 params = {
     'input': {
         'path': resource_filename(__name__, 'data/input/'),
-        'file': {
-            'mensual': 'Mensual.xlsx',
-            'trimestral': 'Trimestral.xlsx'
+        'mensual': {
+            'file': 'Datos_carga_mensual.xlsx',
+            'series': {
+                'paro': 'Paro',
+                'contratos': 'Contratos',
+                'afiliados': 'Afiliados',
+                'asalariados': 'Afiliados_Asalariados',
+                'no-asalariados': 'Afiliados_No_asalariados',
+                'ipc': 'Ipc',
+                'matriculaciones': 'Matriculacion_turismos'
+            },
+            'sources': {
+                'paro': 'ICANE a partir de Movimiento Laboral Registrado del SEPE',
+                'contratos': 'ICANE a partir de Movimiento Laboral Registrado del SEPE',
+                'afiliados': 'ICANE a partir de microdatos de la TGSS',
+                'asalariados': 'ICANE a partir de microdatos de la TGSS',
+                'no-asalariados': 'ICANE a partir de microdatos de la TGSS',
+                'ipc': 'ICANE a partir del Índice de Precios al Consumo del INE',
+                'matriculaciones': 'ICANE a partir de datos de la DGT'
+            }
+        },
+        'trimestral': {
+            'file': 'Datos_carga_trimestral.xlsx',
+            'series': {
+                'confianza': 'Indice confianza empresarial'
+            },
+            'sources': {
+                'confianza': 'ICANE a partir del Índice de Confianza Empresarial del INE'
+            }
         }
     },
     'output': {
-        'path': resource_filename(__name__, 'data/output/')
+        'path': resource_filename(__name__, 'data/output/'),
+        'mensual': {
+            'files': {
+                'paro': 'paro.json-stat',
+                'paro-tendencia': 'paro-tendencia.json-stat',
+                'contratos': 'contratos.json-stat',
+                'contratos-tendencia': 'contratos-tendencia.json-stat',
+                'afiliados': 'afiliados.json-stat',
+                'afiliados-tendencia': 'afiliados-tendencia.json-stat',
+                'asalariados': 'afiliados-asalariados.json-stat',
+                'asalariados-tendencia': 'afiliados-asalariados-tendencia.json-stat',
+                'no_asalariados': 'afiliados-no-asalariados.json-stat',
+                'no_asalariados-tendencia': 'afiliados-no-asalariados-tendencia.json-stat',
+                'ipc': 'ipc.json-stat',
+                'ipc-tendencia': 'ipc-tendencia.json-stat',
+                'matriculaciones': 'matriculaciones.json-stat',
+                'matriculaciones-tendencia': 'matriculaciones-tendencia.json-stat'
+            }
+        },
+        'trimestral': {
+            'files': {
+                'confianza': 'confianza.json-stat',
+                'confianza-tendencia': 'confianza-tendencia.json-stat'
+            }
+        }
     },
     'month': {
         '1': 'Enero',
@@ -29,7 +79,14 @@ params = {
         '10': 'Octubre',
         '11': 'Noviembre',
         '12': 'Diciembre'
-    }
+    },
+    'quarter': {
+        '1': '1T',
+        '2': '2T',
+        '3': '3T',
+        '4': '4T'
+    },
+    'periods': 61
 }
 
 etl_cfg = Baseconfig(params)
--- etl/main.py
+++ etl/main.py
@@ -10,22 +10,31 @@ from numpy import arange
 from pyjstat import pyjstat
 
 
-def anno_mes(df):
-    """Replace the value of Mes with yyyy - month."""
+def transform(df, time_columns, labels):
+    """Slice dataframe. Generate time period column.
+    
+        df (dataframe): dataset
+        time_columns (list): [year_column, month|quarter_column]
+        labels (dict): time period labels
+    """
+    df = df.tail(etl_cfg.periods)
     df.reset_index(inplace=True)
-    for i in range(1, len(df)):
-        anno = str(df.loc[i, 'Año'])
-        mes = str(df.loc[i, 'Mes'])
-        df.loc[i, 'anno_mes'] =  anno + ' - ' + etl_cfg.month[mes]
-    df.drop('Año', axis=1, inplace=True)
-    df.drop('Mes', axis=1, inplace=True)
-    df.rename(columns={'anno_mes': 'Mes'}, inplace=True)
+
+    for i in range(0, len(df)):
+        period1 = str(df.loc[i, time_columns[0]])
+        period2 = str(df.loc[i, time_columns[1]])
+        df.loc[i, 'period'] =  period1 + ' - ' + \
+            labels[str(df.loc[i, time_columns[1]])]
+
+    df.drop(columns={time_columns[0], time_columns[1]}, axis=1, inplace=True)
+    df.rename(columns={'period': time_columns[1]}, inplace=True)
     return df
 
 def to_json(df, id_vars, value_vars,
             source='Instituto Cántabro de Estadística'):
     """Export dataframe to JSON-Stat dataset.
     
+        df (dataframe): dataset
         id_vars (list): index columns
         value_vars (list): numeric variables (metrics)
     """
@@ -45,74 +54,21 @@ def write_to_file(json_data, file_name):
     file.write(json_data)
     file.close()
 
+"""Comienzo del proceso."""
 data = xlsx(etl_cfg.input.path)
 
-# Indicadores de paro registrado mensual
-paro = data[etl_cfg.input.file.mensual]['Hoja1'][[
-    'Año',
-    'Mes',
-    'Paro Cantabria',
-    'Paro España'
-]].copy()
-paro = paro.tail(61)
-paro = anno_mes(paro)
-json_file = to_json(
-    paro,
-    ['Mes'],
-    ['Paro Cantabria', 'Paro España'],
-    'ICANE a partir de Movimiento Laboral Registrado del SEPE')
-write_to_file(json_file, etl_cfg.output.path + 'paro_registrado.json-stat')
+"""Series trimestrales."""
 
-paro_tendencia = data[etl_cfg.input.file.mensual]['Hoja1'][[
-    'Año',
-    'Mes',
-    'Paro Cantabria. Var interanual',
-    'Paro Cantabria. Tendencia',
-    'Paro España. Var interanual',
-    'Paro España. Tendencia'
-]].copy()
-paro_tendencia = paro_tendencia.tail(61)
-paro_tendencia = anno_mes(paro_tendencia)
+# Confianza empresarial
+confianza = data[etl_cfg.input.trimestral.file]\
+    [etl_cfg.input.trimestral.series.confianza][[
+    'Año', 'Trimestre', 'Dato Cantabria', 'Dato España']].copy()
+confianza = transform(confianza, ['Año', 'Trimestre'], etl_cfg.quarter)
 json_file = to_json(
-    paro_tendencia,
-    ['Mes'],
-    ['Paro Cantabria. Var interanual', 'Paro Cantabria. Tendencia',
-     'Paro España. Var interanual', 'Paro España. Tendencia'],
-    'ICANE a partir de Movimiento Laboral Registrado del SEPE')
-write_to_file(json_file, etl_cfg.output.path + 'paro_registrado_tendencia.json-stat')
-
-# Indicadores de contratos registrados mensuales
-contratos = data[etl_cfg.input.file.mensual]['Hoja1'][[
-    'Año',
-    'Mes',
-    'Contratos Cantabria',
-    'Contratos España'
-]].copy()
-contratos = contratos.tail(61)
-contratos = anno_mes(contratos)
-json_file = to_json(
-    contratos,
-    ['Mes'],
-    ['Contratos Cantabria', 'Contratos España'],
-    'ICANE a partir de Movimiento Laboral Registrado del SEPE')
-write_to_file(json_file, etl_cfg.output.path + 'contratos_registrados.json-stat')
-
-contratos_tendencia = data[etl_cfg.input.file.mensual]['Hoja1'][[
-    'Año',
-    'Mes',
-    'Contratos Cantabria. Var interanual',
-    'Contratos Cantabria. Tendencia',
-    'Contratos España. Var interanual',
-    'Contratos España. Tendencia'
-]].copy()
-contratos_tendencia = contratos_tendencia.tail(61)
-contratos_tendencia = anno_mes(contratos_tendencia)
-json_file = to_json(
-    contratos_tendencia,
-    ['Mes'],
-    ['Contratos Cantabria. Var interanual', 'Contratos Cantabria. Tendencia',
-     'Contratos España. Var interanual', 'Contratos España. Tendencia'],
-    'ICANE a partir de Movimiento Laboral Registrado del SEPE')
-write_to_file(json_file, etl_cfg.output.path + 'contratos_registrados_tendencia.json-stat')
+    confianza,
+    ['Trimestre'],
+    ['Dato Cantabria', 'Dato España'],
+    etl_cfg.input.trimestral.sources.confianza)
+write_to_file(json_file, etl_cfg.output.path + etl_cfg.output.trimestral.files.confianza)
 
 print('Fin del proceso. Ficheros generados con éxito')
