5e11637049b366190fd390658bc8ec6e4733102e
Mario Señas Gómez <masego24@users.noreply.github.com>
2021-01-05 10:55:15 +0100

    Corrige estilo del codigo
    Se corrigen errores en el estilo del codigo advertidos por linters

--- etl/quarterly.py
+++ etl/quarterly.py
@@ -1,19 +1,19 @@
 """Quarterly indicators."""
 
-from etl.common import to_json_stat, write_to_file
+import json
 
+from etl.common import to_json_stat, write_to_file
 from etl.config_quarterly import quarterly_cfg as cfg
 
 from etlstat.extractor.extractor import xlsx
-
-import json
 
 import pandas as pd
 
 
 def transform(df, periods, prefix=''):
-    """Slice dataframe. Generate time period column.
-    
+    """
+    Slice dataframe. Generate time period column.
+
         df (dataframe): dataset
         periods (int): number of time periods
         prefix (str): prefix for time periods
@@ -21,13 +21,14 @@ def transform(df, periods, prefix=''):
     for i in range(0, len(df)):
         period1 = str(df.loc[i, 'Año'])
         period2 = str(df.loc[i, 'Trimestre'])
-        df.loc[i, 'period'] =  prefix + period1 + '-' + period2
+        df.loc[i, 'period'] = prefix + period1 + '-' + period2
 
     df.drop(columns={'Año', 'Trimestre'}, axis=1, inplace=True)
     df.rename(columns={'period': 'Trimestre'}, inplace=True)
     df = df.tail(periods)
     df = df.round(2)
     return df
+
 
 def replace_quarter(json_str):
     """Replace quarter number by its label."""
@@ -36,6 +37,7 @@ def replace_quarter(json_str):
     json_str = json_str.replace('-3"', '-3T"')
     json_str = json_str.replace('-4"', '-4T"')
     return json_str
+
 
 # Read  input files
 data = xlsx(cfg.path.input)
@@ -55,27 +57,29 @@ for key in cfg.series:
         inplace=True)
     if cfg.series[key].rate_vars != []:
         data[cfg.file][cfg.series[key].sheet].rename(
-        columns={
-            cfg.series[key].rate_vars[0]: 'Var. interanual Cantabria',
-            cfg.series[key].rate_vars[1]: 'Var. interanual España'},
-        inplace=True)
+            columns={
+                cfg.series[key].rate_vars[0]: 'Var. interanual Cantabria',
+                cfg.series[key].rate_vars[1]: 'Var. interanual España'},
+            inplace=True)
     if cfg.series[key].trend_vars != []:
         data[cfg.file][cfg.series[key].sheet].rename(
-        columns={
-            cfg.series[key].trend_vars[0]: 'Tendencia Cantabria',
-            cfg.series[key].trend_vars[1]: 'Tendencia España'},
-        inplace=True)
+            columns={
+                cfg.series[key].trend_vars[0]: 'Tendencia Cantabria',
+                cfg.series[key].trend_vars[1]: 'Tendencia España'},
+            inplace=True)
 
     # Remove .0 from Año and Trimestre
     data[cfg.file][cfg.series[key].sheet]['Año'] = \
-        data[cfg.file][cfg.series[key].sheet]['Año'].astype(str).replace('\.0', '', regex=True)
+        data[cfg.file][cfg.series[key].sheet]['Año'].astype(str).replace(
+            r'\.0', '', regex=True)
     data[cfg.file][cfg.series[key].sheet]['Trimestre'] = \
-        data[cfg.file][cfg.series[key].sheet]['Trimestre'].astype(str).replace('\.0', '', regex=True)
+        data[cfg.file][cfg.series[key].sheet]['Trimestre'].astype(str).replace(
+            r'\.0', '', regex=True)
 
     # Value variables
     variables = ['Año', 'Trimestre', 'Valor Cantabria', 'Valor España']
-    df = data[cfg.file]\
-        [cfg.series[key].sheet][variables].copy()
+    df = data[cfg.file][
+        cfg.series[key].sheet][variables].copy()
     df = transform(df, cfg.periods.quarterly)
     json_file = to_json_stat(
         df,
@@ -93,16 +97,16 @@ for key in cfg.series:
     # Rate and trend vars
     if cfg.series[key].rate_vars != []:
         if cfg.series[key].trend_vars == []:
-                variables = [
+            variables = [
                 'Año', 'Trimestre', 'Var. interanual Cantabria',
-                    'Var. interanual España']
+                'Var. interanual España']
         else:
             variables = [
                 'Año', 'Trimestre', 'Var. interanual Cantabria',
-                    'Var. interanual España', 'Tendencia Cantabria',
-                    'Tendencia España']
-        df_trend = data[cfg.file]\
-            [cfg.series[key].sheet][variables].copy()
+                'Var. interanual España', 'Tendencia Cantabria',
+                'Tendencia España']
+        df_trend = data[cfg.file][
+            cfg.series[key].sheet][variables].copy()
         df_trend = transform(
             df_trend, cfg.periods.quarterly)
         variables.remove('Año')
@@ -135,7 +139,8 @@ for key in cfg.series:
         # Cantabria
         df_cant = data[cfg.file][cfg.series[key].sheet][[
             'Año', 'Trimestre', coltoshow]].copy()
-        df_cant = transform(df_cant, cfg.periods.global_quarterly, 'Cantabria - ')
+        df_cant = transform(
+            df_cant, cfg.periods.global_quarterly, 'Cantabria - ')
         df_cant.set_index('Trimestre', inplace=True)
         df_cant = df_cant.transpose()
         df_cant.insert(0, 'Categoria', cfg.series[key].category)
